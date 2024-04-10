8ea2dd070587ca1e5165e86d8f7ab11e18788d03
AidenWick <aiden.wick@outlook.com>
2024-04-10 15:42:44 -0600

    Updated FrontEnd
    Mostly css upgrade, more to come

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/header'; // Ensure the path matches the file name exactly, including case sensitivity
 import Column from './components/column';
+import './App.css'
+
 
 function App() {
   return (
-    <div>
+    <div className ="appContainer">
       <Header />
       <Column />
     </div>
